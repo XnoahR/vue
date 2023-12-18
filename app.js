26f250d9d171bce235beb0005a153ca68f7a14dd
Rio Saputro <rio.ae23@gmail.com>
2023-12-18 20:00:10 +0700

    C2 - Data and Templates

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 const app = Vue.createApp({
     // template: `<h2>This is a template from app.js</h2>`
+    data() {
+        return {
+            name : 'Raychi',
+            age : 21,
+            job : 'Student',
+        }
+    }
 })
 
 app.mount('#app')
